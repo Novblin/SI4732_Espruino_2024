6fe90688ed159dbe0583c0e08f0a61bcac653db4
Novblin <vladimir.novikov@mail.ru>
2024-12-28 17:43:30 +0200

    Update amapp.js
    This file is only changed in the interface. If I'm confused about my versions, the best version of all can be found in the new flash image, the flash image attached to my fork.

--- ats100/amapp.js
+++ ats100/amapp.js
@@ -3,39 +3,40 @@ eval(STOR.read("button.js"));
 eval(STOR.read("bardisp.js"));
 eval(STOR.read("freqdisp.js"));
 
-var VOL=32;
+
+var VOL=30;
 var BRIGHT=40;
 var SCREENSAVE=120;
-var STATE=0;  //0 = SELECT, 1 = VOL, 2= FREQ, 3 = STATION, 4 = BRIGHTNESS
-var FREQ = 198;
+var STATE=1;  //0 = SELECT, 1 = VOL, 2= FREQ, 3 = STATION, 4 = BRIGHTNESS
+var FREQ = 225;
 var RSSI =0;
-var SNR =0;
 var BANDNAME ="LW";
 var LOWBAND = 130;
 var HIGHBAND = 279;
-var STEP = 9;
+var STEP = 1;
 var BWindex = 1;
 var CAP =0;
 
 
 var BANDS = (STOR.readJSON("bands.json")||[]).filter((e)=>{return e.mod=="AM";});
 
-var BANDSEL = new Selector(BANDS,148,83,(b)=>{STATE = b?3:0;});
-var VOLDISP = new BarDisp("Vol:",28,62,VOL,(b)=>{STATE = b?1:0;});
-var BRIGHTDISP = new BarDisp("BL :",28,72,BRIGHT,(b)=>{STATE = b?4:0;});
-var FREQDISP = new FreqDisp("KHz",50,19,115,28,0,5,FREQ,(b)=>{STATE = b?2:0;},(f)=>{findBand(f);});
-var SCANUP = new Button("Scan+",0,  111, 44, 23, ()=>{scan(true,SCANUP,SCANDOWN);},12);
-var SCANDOWN = new Button("Scan-",49, 111, 44, 23, ()=>{scan(false,SCANDOWN,SCANUP);},12);
-var STEPSET =  new Button("9",  0,94, 44, 12,(b)=>{changeStep(b);},8);
-var BWSET =  new Button("4.0",  49,94, 44, 12,(b)=>{changeBW(b);},8);
-var AGCCTL = new Button("agc on",  98,94, 44, 12,(b)=>{changeAGC(b);},8);
+var BANDSEL = new Selector(BANDS,148,73,(b)=>{STATE = b?3:0;});
+var VOLDISP = new BarDisp("Vol:",26,56,VOL,(b)=>{STATE = b?1:0;});
+var FREQDISP = new FreqDisp("KHz",80,19,115,28,0,5,FREQ,(b)=>{STATE = b?2:0;},(f)=>{findBand(f);});
+var SCANUP = new Button("Scan+",0,  111, 44, 23, ()=>{scan(true,SCANUP,SCANDOWN);},14);
+var SCANDOWN = new Button("Scan-",49, 111, 44, 23, ()=>{scan(false,SCANDOWN,SCANUP);},14);
+var MUTE = new Button("Mute" ,98,111, 44, 23, (b)=>{RADIO.mute(b);},14),
+var STEPSET =  new Button(STEP,  0,83, 44, 23,(b)=>{changeStep(b);},14);
+var BWSET =  new Button("4.0",  49,83, 44, 23,(b)=>{changeBW(b);},14);
+var AGCCTL = new Button("Agc m",  98,83, 44, 23,(b)=>{changeAGC(b);},14);
 var ITEMS=[
-    FREQDISP, VOLDISP,BRIGHTDISP, STEPSET, BWSET, AGCCTL, BANDSEL,SCANUP, SCANDOWN,  
-    new Button("Mute" ,98,111, 44, 23, (b)=>{RADIO.mute(b);},12),
+    FREQDISP, VOLDISP, STEPSET, BWSET, AGCCTL,SCANUP, SCANDOWN, MUTE, BANDSEL	
 ]; 
+var agcindex = 3; // AGC mid
+const agcstr = ["Agc", "off", "low", "mid", "high"];
 
-var agcindex = 0;
-const agcstr = ["agc on", "off", "low", "med", "high"]
+VOLDISP.toggle(true);
+
 function changeAGC(b){
   if (!b) return;
   agcindex = (agcindex+1)%5;
@@ -47,13 +48,13 @@ function changeAGC(b){
   AGCCTL.reset();
 }
     
-var stepindex= 2;
+var stepindex= 0;
 const steps =[1,5,9,10];
 function changeStep(b){
   if (!b) return;
-  stepindex = (stepindex+1)%4;
   STEP=steps[stepindex];
   STEPSET.str = STEP.toString();
+  stepindex = (stepindex+1)%4;
   STEPSET.reset();
 }
 
@@ -67,16 +68,14 @@ function changeBW(b,index){
 }
 
 function drawBand() {
-  g.setColor(Yellow).setFont('6x8').setFontAlign(-1,-1);
-  g.drawString("MIN: "+LOWBAND+"KHz   ",148,62,true);
-  g.drawString("MAX: "+HIGHBAND+"KHz   ",148,72,true);
-  STEPSET.str = STEP.toString(); STEPSET.draw();
+  g.setColor(Yellow).setFont("Vector",12).setFontAlign(-1,-1);
+  g.drawString(" "+LOWBAND+"   ",105,55,true);
+  g.drawString("- "+HIGHBAND+" KHz  ",155,55,true);
 }
 
 function drawSignal(){
   g.setColor(Yellow);
-  g.setFont('6x8').setFontAlign(-1,-1).drawString("RSSI: "+RSSI+"   ",0,0,true);
-  g.setFontAlign(-1,-1).drawString("SNR : "+SNR+" ",95,0,true);
+  g.setFont("Vector",12).setFontAlign(-1,-1).drawString("RSSI: "+RSSI+"   ",0,0,true);
 } 
 
 
@@ -87,6 +86,8 @@ function setBand(f) {
     LOWBAND =bd.min;
     HIGHBAND=bd.max;
     STEP=bd.step;
+	STEPSET.str = STEP.toString();
+    STEPSET.reset();
     if (f) FREQ=f; else FREQ=bd.freq;
     CAP= (bd.name=="LW" || bd.name=="MW")?0:1;
   }
@@ -110,13 +111,14 @@ function setTune(f){
   RADIO.tuneAM(f,CAP);
   while(!RADIO.endTune());
   var r= RADIO.getTuneStatus();
-  FREQ=r.freq; SNR=r.snr; RSSI=r.rssi;
+  FREQ=r.freq; 
+  RSSI=r.rssi;
   FREQDISP.update(FREQ);
   drawSignal();
 }
 
 var SCANNER=null;
-  
+
 function scan(up,thisone,other){
   if (SCANNER) SCANNER=clearInterval(SCANNER);
   if(other.press){other.reset();}
@@ -125,7 +127,8 @@ function scan(up,thisone,other){
       if (!RADIO.endTune()) return;
       if (SCANNER) SCANNER=clearInterval(SCANNER);
       var r=RADIO.getTuneStatus();
-      FREQ=r.freq; SNR=r.snr; RSSI=r.rssi;
+      FREQ=r.freq; 
+	  RSSI=r.rssi;
       FREQDISP.update(FREQ); drawSignal();
       thisone.reset();
    },100);
@@ -137,9 +140,12 @@ function initRADIO(){
     RADIO.setProp(0xFF00,0); //turn off debug see AN332 re noise
     RADIO.setProp(0x3102,BWindex);
     RADIO.setProp(0x3302,0);
+    RADIO.setProp(0x3103,7800); //max gain AVC
+    RADIO.setProp(0x3403,4); //S/N dB AM_SEEK_TUNE_SNR_THRESHOLD
+    RADIO.setProp(0x3404,1); //dBuV AM_SEEK_TUNE_RSSI_THRESHOLD
     RADIO.volume(VOL);
     setBand();
-    RADIO.setAGC(true,0);
+    RADIO.setAGC(false,(agcindex-1)*3);
 }
 
 var prevpos =0;
@@ -166,22 +172,17 @@ function setControls(){
             FREQ = FREQ<LOWBAND?LOWBAND:FREQ>HIGHBAND?HIGHBAND:FREQ;
             setTune(FREQ);
         } else if(STATE==1) {
-            VOL+=inc*4;
+            VOL+=inc*(7);
             VOL=VOL<0?0:VOL>63?63:VOL;
             VOLDISP.update(VOL);
             RADIO.volume(VOL);
-        } else if(STATE==4) {
-            BRIGHT+=inc*4;
-            BRIGHT=BRIGHT<5?5:BRIGHT>63?63:BRIGHT;
-            BRIGHTDISP.update(BRIGHT);
-            brightness(BRIGHT/63);
         } else if (STATE==3) {
           BANDSEL.move(inc);
           if (BANDS.length!=0) setBand();
         } 
-        SCREENSAVE = 120;    
+        SCREENSAVE = 120;
     };
-    ROTARY.on("change",ROTARY.handler);  
+    ROTARY.on("change",ROTARY.handler);
     BUTTON.on("change",(d)=>{
       if (SCREENSAVE<=0) brightness(BRIGHT/63); else ITEMS[position].toggle(d);
       SCREENSAVE = 120;
@@ -192,11 +193,17 @@ function setControls(){
 var s;
 
 function restoreState(){
-   s = STOR.readJSON("amstate.json",1)||{frequency:1024, volume:32, bright:40, bandwidth:1};
+   s = STOR.readJSON("amstate.json",1)||{frequency:1024, volume:30, bright:40, bandwidth:1,STEP:1};
    VOL=s.volume; VOLDISP.update(VOL); RADIO.volume(VOL);
-   BRIGHT=s.bright; BRIGHTDISP.update(BRIGHT); brightness(BRIGHT/63);
    changeBW(true,s.bandwidth);
    findBand(s.frequency);
+   STEP=s.step;
+   STEPSET.str = STEP.toString();
+   STEPSET.reset();
+   
+   s = STOR.readJSON("brightness.json",1)||{brigh:43};
+   BRIGHT=s.bright; 
+   brightness(BRIGHT/63);
 }
 
 function saveState(){
@@ -204,6 +211,7 @@ function saveState(){
   s.volume=VOL;
   s.bright=BRIGHT;
   s.bandwidth=BWindex;
+  s.step=STEP;
   STOR.writeJSON("amstate.json",s);
 }
 
@@ -212,19 +220,26 @@ E.on("kill",saveState);
 g.clear();
 for (var i=0;i<ITEMS.length;i++) 
   ITEMS[i].focus(i==position);
-g.setColor(Yellow).setFont('6x8').setFontAlign(-1,-1).drawString("Step",0,83).drawString("BWid",49,83);
+g.setColor(Yellow).setFont('6x8').setFontAlign(-1,-1).drawString("Step",5,74).drawString("BWid",55,74);
 setControls();
 initRADIO();
 restoreState();
+
+
 setInterval(()=>{
-  var r = RADIO.getSQ();
-  SNR=r.snr; RSSI=r.rssi; STEREO=r.stereo;
-  drawSignal();
-  g.setFontAlign(-1,-1).drawString("BAT: "+getBattery().toFixed(1)+"V",180,0,true);
   if (SCREENSAVE>0) {
     --SCREENSAVE;
-    if (SCREENSAVE<=0){
-      brightness(0);
-    }
-  } 
+      var r = RADIO.getSQ();
+	  r = RADIO.getSQ();
+	  RSSI=r.rssi; 
+      drawSignal();
+ 	  var v=getBattery().toFixed(1);
+	  if (getBattery()<3.25){g.setColor(Orange);} else {g.setColor(Green);}
+      g.setFontAlign(-1,-1).drawString("BAT: "+v+"V",180,0,true);
+	  g.setColor(Yellow);
+  }	
+  else{
+       brightness(0);
+      }
+
 },1000);
