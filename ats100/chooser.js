7cf316dfdce8a884022b0e940c6923f2e3602bd0
Novblin <vladimir.novikov@mail.ru>
2024-12-28 17:02:30 +0200

    Update chooser.js
    The Brightness adjusting moved from differend radios in start menu , radios interfaces claried.
    Many other changes in other files you can find in the combined new .bin file in my forke. I will publicate new commits for all changets.

--- ats100/chooser.js
+++ ats100/chooser.js
@@ -1,16 +1,47 @@
+
 g.clear();
 g.drawImage(STOR.read("smallradio"),10,5);
 g.setColor(-1).setFont("Vector",18).drawString("Espruino Radio",70,30);
 eval(STOR.read("button.js"));
+
+var BRIGHT=40;
+
 var FMBUTTON = new Button("FM",0,70,50,30,()=>{load("fmapp.js");},20);
 var AMBUTTON = new Button("AM",60,70,50,30,()=>{load("amapp.js");},20);
 var SSBUTTON = new Button("SSB",120,70,50,30,()=>{load("ssbapp.js");},20);
-var OFFBUTTON = new Button("OFF",180,70,50,30,()=>{load();},20);
+var BRBUTTON = new Button("BR",180,70,50,30,()=>{changeBR();},20);
 
-var ITEMS=[FMBUTTON,AMBUTTON,SSBUTTON,OFFBUTTON];
+var ITEMS=[FMBUTTON,AMBUTTON,SSBUTTON,BRBUTTON];
+var prevpos = 3;
+var position = 3;
 
-var prevpos =3;
-var position=3;
+var v=getBattery().toFixed(1);
+if (getBattery()<3.3){g.setColor(Orange);} else {g.setColor(Green);}
+g.setFont("Vector",12).setFontAlign(-1,-1).drawString("BAT: "+v+"V",180,0,true);
+
+var s = STOR.readJSON("brightness.json",1)||{"bright":10};
+BRIGHT = s.bright;
+brightness(BRIGHT/63);
+
+function drawbr(){
+	BUTTON.press = false;
+	BRBUTTON.reset();
+	BUTTON.fn = false;	
+	var br=Math.round((BRIGHT/63)*100);
+	g.setColor(-1).setFont("Vector",18).setFontAlign(0,-1).drawString("                   "+br+"%",120,110,true);
+}
+drawbr();
+
+function changeBR (inc){
+	BRBUTTON.reset();
+	if (BRIGHT<3){ BRIGHT =3;}
+	BRIGHT = BRIGHT+15;
+	if (BRIGHT>63){ BRIGHT =3;}
+	brightness(BRIGHT/63);
+    drawbr();
+	s.bright = BRIGHT; 
+    STOR.writeJSON("brightness.json",s);
+};
 
 function move(inc){
         function mod(a,n) {return a>=n?a-n:a<0?a+n:a;}
@@ -19,11 +50,10 @@ function move(inc){
         ITEMS[position].focus(true);
         prevpos=position;
     }
-
-ROTARY.on("change",(inc)=>{move(inc);});  
 BUTTON.on("change",(d)=>{ITEMS[position].toggle(d);});
 
-g.setColor(-1).setFont("6x8",1).setFontAlign(0,-1).drawString("Long touch to return to this screen.",120,110);
+ROTARY.on("change",(inc)=>{move(inc);});  
 
-for (var i=0;i<ITEMS.length;i++) 
+for (var i=0;i<ITEMS.length;i++)	
   ITEMS[i].focus(i==position);
+
